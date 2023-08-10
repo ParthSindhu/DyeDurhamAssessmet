d85315ee0fb4c8c3939f6eda1f3c55835f37831c
Parth Sindhu <sindhu.parth@gmail.com>
2023-08-10 16:10:11 -0400

    Initial commit

---
+++ Assesment/App/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Read provided input file
+        string[] input;
+        FileStream fileStream = new(args[0], FileMode.Open);
+        using (StreamReader reader = new(fileStream))
+        {
+            string text = reader.ReadToEnd();
+            input = text.Split(Environment.NewLine);
+        }
+
+        
+
+        // Outputting Sorted names to file.
+        using (StreamWriter outputFile = new StreamWriter("sorted-names-list.txt"))
+        {
+            foreach (string line in input)
+                outputFile.WriteLine(line);
+        }
+    }
+}
